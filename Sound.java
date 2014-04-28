d3cb8609441f191a02271a1aa72d37b24560299c
Adrian Lopez-Perez <lop12002@byui.edu>
2014-04-27 23:38:57 -0600

    Create Sound.java

---
+++ Sound.java
@@ -0,0 +1,16 @@
+/*
+ Try this 2-1
+Compute the distance to a lighting
+strike whose sound takes 7.2 seconds to reach you
+ */
+
+class Sound {
+    public static void main (String args[]){
+        double dist;
+        
+        dist = 7.2*1100;
+        
+        System.out.println("The lighning is " + dist + " feet away.");
+        
+        } 
+    }
